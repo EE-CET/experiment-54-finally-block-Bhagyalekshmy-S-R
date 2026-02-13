13c8b29a6bc67815968460a26c95dc34b4fb6806
Your Name <you@example.com>
2026-02-13 10:17:17 +0530

    first commit

--- src/FinallyDemo.java
+++ src/FinallyDemo.java
@@ -2,16 +2,21 @@ import java.util.Scanner;
 
 public class FinallyDemo {
     
-        // TODO: Read a and b
-        
-        // TODO: try block
-        // Perform division
-        // Print "Result: " + result
-        
-        // TODO: catch ArithmeticException e
-        // Print "Error: " + e.getMessage()
-        
-        // TODO: finally block
-        // Print "Finally block executed"
-    }
+       public static void main(String[] args){
+            Scanner sc =new Scanner(System.in);
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int result=0;
+            try{
+              result=a/b;
+              System.out.println("Result: "+result);
+               }
+           catch(ArithmeticException e){
+               System.out.println("Error: / by zero");
+                }
+           finally{
+           System.out.println("Finally block executed");
+           }
+         sc.close();
+     }
 }
